5f53ce920d1c075a886cb3d986a5ef5fd6974dd6
Makera kaba <makerakaba02@gmail.com>
2024-08-28 02:45:35 +0000

    authentification

--- EmploiApp/urls.py
+++ EmploiApp/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('',views.home, name="home"),
+    path('auth',views.auth, name="auth"),
+    path('auth2',views.auth2, name="auth2"),
 ]
 
--- EmploiApp/views.py
+++ EmploiApp/views.py
@@ -5,3 +5,10 @@ from django.shortcuts import render
 def home(request):
     
     return render(request,'home.html')
+
+
+def auth(request):
+    return render(request,"auth/auth.html" )
+
+def auth2(request):
+    return render(request,"auth/auth2.html")
