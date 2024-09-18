12eb7df2dd36eaccc5464674e40cdc1f651695e4
etikam <etiennedheleine2000@gmail.com>
2024-09-18 02:49:28 +0000

    ordonnancer des jours de la semaine courante

--- EmploiApp/views.py
+++ EmploiApp/views.py
@@ -1,12 +1,39 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+
+
+from .models import Seance
+from datetime import datetime, timedelta
+
 # from account.views import signin
 # Create your views here.
 
 
-login_required(login_url='account:app_login')
+@login_required(login_url='account:app_login')
 def home(request):
+    # Récupérer la date actuelle
+ 
+    current_date = datetime.now().date()
+    print(f'currente date: {current_date}')
+
+    # Calculer les dates des 7 jours de la semaine
+    start_of_week = current_date - timedelta(days=current_date.weekday())
+
+    days_of_week = [start_of_week + timedelta(days=i) for i in range(7)]
+
+    # Récupérer le paramètre de la date
     
-    return render(request,'home.html')
+    selected_date = request.GET.get('date', current_date)
+    
+    # Récupérer les séances pour la date sélectionnée
+    seances = Seance.objects.filter(start_time__date=selected_date)
+
+    context = {
+        'days_of_week': days_of_week,
+        'seances': seances,
+        'selected_date': selected_date,
+    }
+    return render(request,'home.html',context)
+
 
 
